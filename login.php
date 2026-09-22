5c5bd4d78f08265eb6034acbf31d7c191c9ca19d
Lucas Strossi <lucasstrossi4@gmail.com>
2026-09-22 08:58:57 -0300

    inicio do formulario

--- login.php
+++ login.php
@@ -7,9 +7,15 @@
 </head>
 <body>
     <form action="" method="post">
-        <input type="text" placeholder="E-mail">
-        <input type="password" placeholder="senha">
+        <input type="text" placeholder="E-mail" name="email">
+        <input type="password" placeholder="senha" name="senha">
         <button>Logar</button>
     </form>
 </body>
 </html>
+<?php
+$email = $_POST['email']?? false;
+$senha = $_POST['senha']?? false;
+
+
+?>
